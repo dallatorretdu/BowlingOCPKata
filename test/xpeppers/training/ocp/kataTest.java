d80363cd380e68509b20f905eab090dec4eed248
Giulio Dallatorre <giulio.dallatorre@xpeppers.com>
2015-03-12 10:44:30 +0100

    first commit

---
+++ test/xpeppers/training/ocp/kataTest.java
@@ -0,0 +1,14 @@
+package xpeppers.training.ocp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class kataTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
